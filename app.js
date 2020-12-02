04c46dc598cec72112f0264334c3869627ad597a
Newsha Nik <nioosha.h@gmail.com>
2020-12-03 00:45:14 +0330

    first commit. so far, nothing too hard. just listening on a server port and running our app in the console. I'm just at the beginning of NPM packages chapter whilst committing this.

---
+++ app.js
@@ -0,0 +1,14 @@
+// console.log("PROCESS: ", process);
+
+const helpers = require("./helpers");
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("hello world from node js");
+});
+
+server.listen(3000);
+
+const total = helpers.sum(10, 20);
+console.log(total);
