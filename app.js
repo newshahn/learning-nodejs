1e34314c7defa62d5d61edc93eb931413f082b86
Newsha Nik <nioosha.h@gmail.com>
2020-12-03 01:51:03 +0330

    added package.json by using npm init. tried to install nodemon but must've failed somewhere cause it's not working. added a custom script.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const helpers = require("./helpers");
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("hello world from node js");
+  res.end("hello world from node js updated");
 });
 
 server.listen(3000);
